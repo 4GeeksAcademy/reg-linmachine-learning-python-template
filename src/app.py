ac7e4469a9a23ade674ec13daff176027626fd3b
sebaslop-afk <sebastianlopez8@hotmail.com>
2025-05-12 20:34:13 +0000

    Agrega Flask app y ruta principal

--- src/app.py
+++ src/app.py
@@ -1,4 +1,15 @@
-from utils import db_connect
-engine = db_connect()
+
 
 # your code here
+from flask import Flask, render_template
+from utils import db_connect
+
+engine = db_connect()
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Puedes agregar más rutas si lo necesitas
